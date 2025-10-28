7775a3e796dbb68a6519a1cf7defd52bd073bec2
leogomeslima <leonardo.glima2017@gmail.com>
2025-10-28 20:35:48 -0300

    Updated do arquivo

--- Sispat.Domain/Interfaces/IAssetRepository.cs
+++ Sispat.Domain/Interfaces/IAssetRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sispat.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Sispat.Domain.Interfaces
 {
-    internal interface IAssetRepository
+    public interface IAssetRepository : IGenericRepository<Asset>
     {
+        // implementar depois métodos específicos para Asset, se necessário
     }
 }
