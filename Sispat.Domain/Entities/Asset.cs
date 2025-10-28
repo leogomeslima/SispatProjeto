9e5bff72a7226ac84d66388067edc8fce20695e8
leogomeslima <leonardo.glima2017@gmail.com>
2025-10-28 19:37:34 -0300

    Updated a entidade principal para o ativo e patrimônio

--- Sispat.Domain/Entities/Asset.cs
+++ Sispat.Domain/Entities/Asset.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Sispat.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sispat.Domain.Entities
 {
-    internal class Asset
+    public class Asset
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty; 
+        public string? Description { get; set; }
+
+        
+        public string SerialNumber { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PurchaseValue { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public AssetStatus Status { get; set; }
+                
+        public Guid CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+                
+        public Guid? LocationId { get; set; }
+        public virtual Location? Location { get; set; }
+               
+        public string? AssignedToUserId { get; set; }
+        public virtual ApplicationUser? AssignedToUser { get; set; }
     }
 }
