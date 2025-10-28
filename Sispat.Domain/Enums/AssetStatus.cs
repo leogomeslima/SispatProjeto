49be1bd5e836ce36387e6c4dbc738bac7f450461
leona <leona@LEO-E-ANY>
2025-10-28 18:41:49 -0300

    Adicionar arquivos de projeto.

---
+++ Sispat.Domain/Enums/AssetStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sispat.Domain.Enums
+{
+    public enum AssetStatus
+    {
+        EmEstoque = 1,
+        EmUso = 2,
+        EMManutencao = 3,
+        Baixado = 4
+    }
+}
