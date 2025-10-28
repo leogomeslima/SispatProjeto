7600def3b19e2bb9c679bef59d3750517f856b71
leogomeslima <leonardo.glima2017@gmail.com>
2025-10-28 19:38:27 -0300

    Updated para localização

--- Sispat.Domain/Entities/Location.cs
+++ Sispat.Domain/Entities/Location.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Sispat.Domain.Entities
 {
-    internal class Location
+    public class Location
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Building { get; set; } = string.Empty;
+        public string Floor { get; set; } = string.Empty;
+        public virtual ICollection<Asset> Assets { get; set; } = new List<Asset>();
     }
 }
