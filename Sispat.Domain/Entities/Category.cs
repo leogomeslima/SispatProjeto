7f78df66e97d7499fd5645c3d85449defdd9880b
leogomeslima <leonardo.glima2017@gmail.com>
2025-10-28 19:39:24 -0300

    updated da categoria

--- Sispat.Domain/Entities/Category.cs
+++ Sispat.Domain/Entities/Category.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Sispat.Domain.Entities
 {
-    internal class Category
+    public class Category
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public virtual ICollection<Asset> Assets { get; set; } = new List<Asset>();
     }
 }
