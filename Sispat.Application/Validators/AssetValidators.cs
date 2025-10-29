253bd4dd267499746a581bba5be36f9a3e5da08a
leogomeslima <leonardo.glima2017@gmail.com>
2025-10-29 01:00:35 -0300

    Adiciona validadores para criação e atualização de ativos
    Inclui as classes `CreateAssetValidator` e `UpdateAssetValidator` para validar objetos `CreateAssetDto` e `UpdateAssetDto`, respectivamente. As validações garantem que os dados dos ativos sejam consistentes e corretos. Ajusta importações de namespaces para suportar as novas validações. Modifica o acesso da classe `AssetValidators` de `internal` para `public`.

--- Sispat.Application/Validators/AssetValidators.cs
+++ Sispat.Application/Validators/AssetValidators.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation;
+using Sispat.Application.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace Sispat.Application.Validators
 {
-    internal class AssetValidators
+    public class CreateAssetValidator : AbstractValidator<CreateAssetDto>
     {
+        public CreateAssetValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("O nome é obrigatório.")
+                .MaximumLength(200);
+
+            RuleFor(x => x.SerialNumber)
+                .NotEmpty().WithMessage("O número de série é obrigatório.");
+
+            RuleFor(x => x.PurchaseValue)
+                .GreaterThan(0).WithMessage("O valor de compra deve ser maior que zero.");
+
+            RuleFor(x => x.PurchaseDate)
+                .NotEmpty()
+                .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("A data da compra não pode ser no futuro.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("O status fornecido não é válido.");
+
+            RuleFor(x => x.CategoryId)
+                .NotEmpty().WithMessage("A categoria é obrigatória.");
+        }
+    }
+
+    public class UpdateAssetValidator : AbstractValidator<UpdateAssetDto>
+    {
+        public UpdateAssetValidator()
+        {
+            // Validações similares ao Create
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
+            RuleFor(x => x.PurchaseValue).GreaterThan(0);
+            RuleFor(x => x.PurchaseDate).NotEmpty().LessThanOrEqualTo(DateTime.UtcNow);
+            RuleFor(x => x.Status).IsInEnum();
+            RuleFor(x => x.CategoryId).NotEmpty();
+        }
     }
 }
